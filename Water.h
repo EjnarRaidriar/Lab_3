b05bae015b6c36f8a9a452412256771dfb1384ab
EjnarRaidriar <emanuil.cucos25@gmail.com>
2022-10-06 15:37:26 +0300

    second commit

---
+++ Water.h
@@ -0,0 +1,11 @@
+#ifndef WATER_H
+#define WATER_H
+
+#include"Map.h"
+
+class Water:public Map
+{
+
+};
+
+#endif
---
+++ Winter.h
@@ -0,0 +1,11 @@
+#ifndef WINTER_H
+#define WINTER_H
+
+#include"Season.h"
+
+class Winter:public Season
+{
+
+};
+
+#endif
